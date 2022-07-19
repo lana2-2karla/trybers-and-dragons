9d243626105dadfd662893effba43a07e6797e25
Allana Karla <allanakarla1702@gmail.com>
2022-07-19 16:38:20 -0300

    :sparkles: feat: criando classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  readonly name: string;
+  readonly dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this.name = name;
+    this.dexterity = dexterity;
+  }
+
+  get getName() {
+    return this.name;
+  }
+
+  get getdexterity() {
+    return this.dexterity;
+  }
+
+  static createdRacesInstances(): string {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
