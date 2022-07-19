f5be20c75484fcf7ce1ad18f78f1ef8f88d41bd1
Allana Karla <allanakarla1702@gmail.com>
2022-07-19 17:54:52 -0300

    :sparkles: feat: criando classe Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  maxLifePoints: number;
+  private static instance = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this.maxLifePoints = 74;
+    Orc.instance += 1;
+  }
+
+  get getMaxLifePoints() {
+    return this.maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.instance;
+  }
+}
+
+export default Orc;
